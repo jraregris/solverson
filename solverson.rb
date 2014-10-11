c54fad956c36b331a8aa5c6ed031902f63053f2a
jraregris <oddmund@oddmundo.com>
2014-10-11 13:10:31 +0200

    1[ |O| ] -> [X|O|X]

--- solverson.rb
+++ solverson.rb
@@ -1,24 +1,40 @@
 class Solverson
   def self.solve_row puzzle
-    clue, grid = puzzle
+    clue, row = puzzle
     if clue.length == 1
       if clue.first == 0
-        return fill_cross(grid)
+        return fill_X(row)
       end
-      if clue.first == grid.length
-        return fill_fill(grid)
+      if clue.first == row.length
+        return fill_O(row)
+      end
+
+      if clue_is_satisfied(clue.first, row)
+      return fill_empty_with_X(row)
       end
     end
+
+    row
   end
   
   private
 
-  def self.fill_cross grid
-    grid.fill :cross
+  def self.fill_X row
+    row.fill :X
   end
 
-  def self.fill_fill grid
-    grid.fill :fill
+  def self.fill_O row
+    row.fill :O
+  end
+
+  def self.fill_empty_with_X row
+    row.map {|c| c == :_ ? :X : :O }
+  end
+  
+  def self.clue_is_satisfied(clue, row)
+    cluestring = 'O'*clue
+    rowstring = row.map { |c| c.to_s }.join
+    rowstring.include? cluestring
   end
 end
 
@@ -27,25 +43,30 @@ require 'minitest/autorun'
 describe Solverson do
   describe 'zero dimensional puzzles' do
     it '0[ ] -> [X]' do
-      puzzle = [[0], [:blank]]
-      Solverson.solve_row(puzzle).must_equal [:cross]
+      puzzle = [[0], [:_]]
+      Solverson.solve_row(puzzle).must_equal [:X]
     end
 
     it '1[ ] -> [O]' do
-      puzzle = [[1],[:blank]]
-      Solverson.solve_row(puzzle).must_equal [:fill]
+      puzzle = [[1],[:_]]
+      Solverson.solve_row(puzzle).must_equal [:O]
     end
   end
 
   describe 'one dimensional puzzles' do
     it '0[ | ] -> [X|X]' do
-      puzzle = [[0], [:blank, :blank]]
-      Solverson.solve_row(puzzle).must_equal [:cross, :cross]
+      puzzle = [[0], [:_, :_]]
+      Solverson.solve_row(puzzle).must_equal [:X, :X]
     end
     
     it '2[ | ] -> [O|O]' do
-      puzzle = [[2],[:blank,:blank]]
-      Solverson.solve_row(puzzle).must_equal [:fill, :fill]
+      puzzle = [[2],[:_,:_]]
+      Solverson.solve_row(puzzle).must_equal [:O, :O]
+    end
+
+    it '1[ |O| ] -> [X|O|X]' do
+      puzzle = [[1], [:_, :O, :_]]
+      Solverson.solve_row(puzzle).must_equal [:X, :O, :X]
     end
   end
 end
