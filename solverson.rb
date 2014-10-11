c585bb39f077f93d87be72240b3dbf4c0ac75912
jraregris <oddmund@oddmundo.com>
2014-10-11 11:50:34 +0200

    1[ ] -> [O]

---
+++ solverson.rb
@@ -0,0 +1,39 @@
+class Solverson
+  def self.solve_row puzzle
+    clue, grid = puzzle
+    if clue.length == 1
+      if clue.first == 0
+        return fill_cross(grid)
+      end
+      if clue.first == 1
+        return fill_fill(grid)
+      end
+    end
+  end
+  
+  private
+
+  def self.fill_cross grid
+    grid.fill :cross
+  end
+
+  def self.fill_fill grid
+    grid.fill :fill
+  end
+end
+
+require 'minitest/autorun'
+
+describe Solverson do
+  describe 'zero dimensional puzzles' do
+    it 'should solve the simplest puzzle' do
+      puzzle = [[0], [:blank]]
+      Solverson.solve_row(puzzle).must_equal [:cross]
+    end
+
+    it 'should solve 1| | -> |O|' do
+      puzzle = [[1],[:blank]]
+      Solverson.solve_row(puzzle).must_equal [:fill]
+    end
+  end
+end
